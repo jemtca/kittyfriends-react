328c4a319408ca51a0872f08cfa86a8b6c67d30d
jemtca <30645648+jemtca@users.noreply.github.com>
2019-03-20 23:50:26 -0700

    Updates

---
+++ precache-manifest.de4314ea98fbb2142048f04d80a7c903.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/kittyfriends-react/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "80de5c0e14c5ef8294b4",
+    "url": "/kittyfriends-react/static/js/runtime~main.2042e159.js"
+  },
+  {
+    "revision": "1e3472093a09021b115d",
+    "url": "/kittyfriends-react/static/js/main.9699d8bf.chunk.js"
+  },
+  {
+    "revision": "248876ac19e7a02bbfed",
+    "url": "/kittyfriends-react/static/js/2.7cb68033.chunk.js"
+  },
+  {
+    "revision": "1e3472093a09021b115d",
+    "url": "/kittyfriends-react/static/css/main.e37dbde0.chunk.css"
+  },
+  {
+    "revision": "248876ac19e7a02bbfed",
+    "url": "/kittyfriends-react/static/css/2.0d413e3e.chunk.css"
+  },
+  {
+    "revision": "5e954d5623248e9cb7dc8b95abf974c8",
+    "url": "/kittyfriends-react/index.html"
+  }
+];
