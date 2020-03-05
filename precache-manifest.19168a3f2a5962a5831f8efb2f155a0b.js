7af9781d69d7cc3bf560e3be0d73d9439a14fb3b
jemtca <30645648+jemtca@users.noreply.github.com>
2020-03-04 19:49:00 -0800

    Updates

---
+++ precache-manifest.19168a3f2a5962a5831f8efb2f155a0b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7f4226245c9a0b94771546d26c1c96f",
+    "url": "/kittyfriends-react/index.html"
+  },
+  {
+    "revision": "9b6257837d2659017646",
+    "url": "/kittyfriends-react/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "be85c5c946b509254a22",
+    "url": "/kittyfriends-react/static/css/main.e4bffd19.chunk.css"
+  },
+  {
+    "revision": "9b6257837d2659017646",
+    "url": "/kittyfriends-react/static/js/2.c0394c3d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/kittyfriends-react/static/js/2.c0394c3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be85c5c946b509254a22",
+    "url": "/kittyfriends-react/static/js/main.fc9654b3.chunk.js"
+  },
+  {
+    "revision": "e9f28028bf27f473c290",
+    "url": "/kittyfriends-react/static/js/runtime-main.ae8b7e7c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/kittyfriends-react/static/media/SEGA.12a3db8b.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kittyfriends-react/precache-manifest.e9d82fbf893234ca8ea5b9cfc1344b6c.js"
+  "/kittyfriends-react/precache-manifest.19168a3f2a5962a5831f8efb2f155a0b.js"
 );
 
 self.addEventListener('message', (event) => {
