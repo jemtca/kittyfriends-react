b3dbf6919a8876ac32ec52418cdb4fc7013c0b19
jemtca <30645648+jemtca@users.noreply.github.com>
2022-04-05 23:18:24 -0700

    dependencies updated

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
+const container = document.getElementById('root');
+const root = createRoot(container);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+root.render(<App />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
